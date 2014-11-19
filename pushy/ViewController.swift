af40134dd94f188ad468db239a657db2e3b4d65f
Mark Madsen <mm@idyll.io>
2014-11-19 10:02:25 -0700

    Initial Commit

---
+++ pushy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pushy
+//
+//  Created by Mark Madsen on 2014-11-19.
+//  Copyright (c) 2014 Mark Madsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
